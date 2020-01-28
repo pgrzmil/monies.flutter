977a27e92efece4381a3b936708110b826426d7d
Pawel Grzmil <pawel.grzmil@gmail.com>
2020-01-28 23:16:29 +0100

    Added widget tests for ExpensesEditView

---
+++ test/widgets/expenseEdit_test.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:monies/data/expensesDataStore.dart';
+import 'package:monies/helpers/expensesDataStoreHelpers.dart';
+import 'package:monies/widgets/expenseEdit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../testWidget.dart';
+
+void main() {
+  ExpensesDataStore expensesProvider;
+
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+    expensesProvider = ExpensesDataStore();
+    ExpensesDataStoreHelpers.loadExpensesFromFile(expensesProvider, 'assets/mockExpenses.json');
+  });
+
+  tearDown(() async {
+    final preferences = await SharedPreferences.getInstance();
+    preferences.clear();
+  });
+
+  testWidgets('Remove expense test', (WidgetTester tester) async {
+    final expense = (await expensesProvider.getAll()).first;
+
+    await tester.pumpWidget(TestWidget(child: ExpenseEditView(expense), expensesProvider: expensesProvider));
+
+    expect((await expensesProvider.getAll()).length, equals(5));
+
+    final removeButton = find.byIcon(Icons.delete);
+    expect(removeButton, findsOneWidget);
+
+    await tester.tap(removeButton);
+    await tester.pumpAndSettle();
+
+    expect((await expensesProvider.getAll()).length, equals(4));
+  });
+
+  testWidgets('Edit expense test', (WidgetTester tester) async {
+    final expense = (await expensesProvider.getAll()).first;
+
+    await tester.pumpWidget(TestWidget(child: ExpenseEditView(expense), expensesProvider: expensesProvider));
+
+    final titleTextField = find.byWidgetPredicate((widget) => widget is TextFormField && widget.initialValue == expense.title);
+    expect(titleTextField, findsOneWidget);
+    await tester.enterText(titleTextField, "test title");
+
+    final locationTextField = find.byWidgetPredicate((widget) => widget is TextFormField && widget.initialValue == expense.location);
+    expect(locationTextField, findsOneWidget);
+    await tester.enterText(locationTextField, "test location");
+
+    final amountTextField = find.byWidgetPredicate((widget) => widget is TextFormField && widget.initialValue == "${expense.amount}");
+    expect(amountTextField, findsOneWidget);
+    await tester.enterText(amountTextField, "543.21");
+
+    final saveButton = find.byIcon(Icons.save);
+
+    await tester.tap(saveButton);
+    await tester.pumpAndSettle();
+
+    final editedExpense = expensesProvider.getById(expense.id);
+    expect(editedExpense.title, equals("test title"));
+    expect(editedExpense.location, equals("test location"));
+    expect("${editedExpense.amount}", equals("543.21"));
+  });
+}
