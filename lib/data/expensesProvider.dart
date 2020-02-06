acdbaf93120152212731e220e80ea2c4a7469858
Pawel Grzmil <pawel.grzmil@gmail.com>
2020-02-07 00:34:48 +0100

    Added expenses sorting and method for retrieving latest n-expenses

--- lib/data/expensesProvider.dart
+++ lib/data/expensesProvider.dart
@@ -6,7 +6,22 @@ class ExpensesProvider extends BaseProvider<Expense> {
 
   @override
   List<Expense> getAll() {
-     items.sort((exp1, exp2) => exp1.date.compareTo(exp2.date));
-     return super.getAll();
+    sortByDate();
+    return super.getAll();
+  }
+
+  void sortByDate() {
+    items.sort((exp1, exp2) => exp1.date.compareTo(exp2.date));
+  }
+
+  List<Expense> getLatest(int count, int month, int year) {
+    //First reverse because take method gets items from the beginning. Second one to keep original order of the items. 
+    return getAll()
+                  .reversed
+                  .where((item) => item.date.month == month && item.date.year == year)
+                  .take(count)
+                  .toList()
+                  .reversed
+                  .toList();
   }
 }
