f7c8890d10ca255022daf5cf0be3267ffd994d11
Pawel Grzmil <pawel.grzmil@gmail.com>
2020-02-10 22:52:04 +0100

    Added sorting categories by order

--- lib/data/categoriesProvider.dart
+++ lib/data/categoriesProvider.dart
@@ -4,5 +4,15 @@ import 'models/category.dart';
 class CategoriesProvider extends BaseStorageProvider<ExpenseCategory> {
   CategoriesProvider() : super(storeKey: "Categories", fromJsonString: ExpenseCategory.fromJsonString);
 
+@override
+  List<ExpenseCategory> getAll() {
+    _sortByOrder();
+    return super.getAll();
+  }
+
+  void _sortByOrder() {
+    items.sort((cat1, cat2) => cat1.order.compareTo(cat2.order));
+  }
+
   String titleFor({String id}) => items.firstWhere((x) => x.id == id, orElse: () => null)?.title;
 }
