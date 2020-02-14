7f01ad2954fca27c7dc7e5fbf41f21936364b9d4
Pawel Grzmil <pawel.grzmil@gmail.com>
2020-02-14 12:49:45 +0100

    Added incomes provider

---
+++ lib/data/incomesProvider.dart
@@ -0,0 +1,17 @@
+import 'models/income.dart';
+import 'extensions/withDate.dart';
+import 'baseStorageProvider.dart';
+
+class IncomesProvider extends BaseStorageProvider<Income> {
+  IncomesProvider() : super(storeKey: "Incomes", fromJsonString: Income.fromJsonString);
+
+  @override
+  List<Income> getAll() {
+    return super.getAll().sortByDate();
+  }
+
+  ///Retrieves incomes for given `month` and `year`
+  Iterable<Income> getForMonth(int month, int year) {
+    return getAll().filterByDate(month, year);
+  }
+}
